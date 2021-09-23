caa5df23df77261887b8b9296b333dbe65002ecb
pico <a.pico@posteo.de>
2021-09-23 20:49:39 +0200

    rl with current position as state

--- scan_gym/scan_gym/envs/ScannerEnv/scanner_env.py
+++ scan_gym/scan_gym/envs/ScannerEnv/scanner_env.py
@@ -14,7 +14,9 @@ import json
 from .utils import *
 import glob
 import os
-from .space_carving import space_carving_2
+from .space_carving import *
+
+MODELS_PATH = '/home/pico/uni/romi/scanner_cube/arabidopsis_im_bigger/'
 
 
 class ScannerEnv(gym.Env):
@@ -30,21 +32,21 @@ class ScannerEnv(gym.Env):
         self.init_pos_inc_rst = init_pos_inc_rst #if false init position is random, if true, it starts in position 0 and increments by 1 position every reset
         self.init_pos_counter = 0
         
-        self.volume_shape = (66,68,152,1)
+        self.volume_shape = (66,68,152)
         self.im_ob_space = gym.spaces.Box(low=-1, high=1, shape=self.volume_shape, dtype=np.float32)
 
         #current position                                          
         #lowl = np.array([0])
         #highl = np.array([179])                                           
         #self.vec_ob_space = gym.spaces.Box(lowl, highl, dtype=np.float32)
-        self.vec_ob_space  = spaces.Discrete(16)
+        self.vec_ob_space  = spaces.Discrete(self.n_positions)
 
 
         self.observation_space = gym.spaces.Tuple((self.im_ob_space, self.vec_ob_space))
 
 
 
-        self.actions = {0:1,1:3,2:5,3:11,4:23,5:45,6:-45,7:-23,8:-11,9:-5,10:-3,11:-1}
+        self.actions = {0:90,1:3,2:5,3:11,4:23,5:45,6:-45,7:-23,8:-11,9:-5,10:-3,11:-90}
         self.action_space = gym.spaces.Discrete(12)
 
         #self._spec.id = "Romi-v0"
@@ -74,9 +76,15 @@ class ScannerEnv(gym.Env):
         self.kept_abs_images.append(self.absolute_position) 
         
 
-        self.spc = space_carving_2(self.dataset_path)
+        if self.dataset_path == '':
+            model = np.random.randint(10) #we use first 10 models from database for training
+            self.spc = space_carving_2_masks( os.path.join(MODELS_PATH,str(model).zfill(3)) )
+        else:
+            self.spc = space_carving_2_masks(self.dataset_path)
+
+            
         self.spc.carve(self.absolute_position) 
-        self.current_state = ( self.spc.sc.values().reshape(self.volume_shape) , 0) #self.spc.sc.values().astype('float16')
+        self.current_state = ( self.spc.sc.values() , 0) #self.spc.sc.values().astype('float16')
 
         #get number of -1's (empty space), 0's (undetermined) and 1's (solid) from 3d volume
         h = np.histogram(self.spc.sc.values(), bins=3)[0]
@@ -124,7 +132,7 @@ class ScannerEnv(gym.Env):
            
         self.total_reward += reward
 
-        self.current_state = ( self.spc.sc.values().reshape(self.volume_shape) , self.current_position )
+        self.current_state = ( self.spc.sc.values() , self.current_position )
 
         return self.current_state, reward, self.done, {}
 
--- scan_gym/scan_gym/envs/ScannerEnv/space_carving.py
+++ scan_gym/scan_gym/envs/ScannerEnv/space_carving.py
@@ -9,7 +9,7 @@ from PIL import Image
 from utils import *
 import glob
 import os
-
+import cv2
 
 
 class space_carving():
@@ -23,8 +23,8 @@ class space_carving():
         self.intrinsics= self.camera_model['params'][0:4]
         
         params = json.load(open(os.path.join(dataset_path, 'params.json')))
-        self.gt=o3d.io.read_point_cloud(params["gt_path"])
-        self.gt_points = np.asarray(self.gt.points)
+        #self.gt=o3d.io.read_point_cloud(params["gt_path"])
+        #self.gt_points = np.asarray(self.gt.points)
         self.n_dilation=params["sc"]["n_dilation"]
         self.voxel_size = params['sc']['voxel_size']
         
@@ -77,13 +77,13 @@ class space_carving():
             for k in range(self.n_dilation): mask = binary_dilation(mask)    
         self.sc.process_view(self.intrinsics, rot, tvec, mask)
         
-    def dist_to_gt(self):
+    '''def dist_to_gt(self):
         vol = self.sc.values().copy()
         vol = vol.reshape(self.sc.shape)
         pcd=proc3d.vol2pcd_exp(vol, self.origin, self.voxel_size, level_set_value=0) 
         pcd_p = np.asarray(pcd.points)
         cd=chamfer_d(self.gt_points , pcd_p)
-        return cd 
+        return cd '''
 
 
 
@@ -97,8 +97,8 @@ class space_carving_2():
         self.intrinsics= self.camera_model['params'][0:4]
         
         params = json.load(open(os.path.join(dataset_path, 'params.json')))
-        self.gt=o3d.io.read_point_cloud(params["gt_path"])
-        self.gt_points = np.asarray(self.gt.points)
+        #self.gt=o3d.io.read_point_cloud(params["gt_path"])
+        #self.gt_points = np.asarray(self.gt.points)
         self.n_dilation=params["sc"]["n_dilation"]
         self.voxel_size = params['sc']['voxel_size']
         
@@ -146,13 +146,79 @@ class space_carving_2():
             for k in range(self.n_dilation): mask = binary_dilation(mask)    
         self.sc.process_view(self.intrinsics, rot, tvec, mask)
         
-    def dist_to_gt(self):
+    '''def dist_to_gt(self):
         vol = self.sc.values().copy()
         vol = vol.reshape(self.sc.shape)
         pcd=proc3d.vol2pcd_exp(vol, self.origin, self.voxel_size, level_set_value=0) 
         pcd_p = np.asarray(pcd.points)
         cd=chamfer_d(self.gt_points , pcd_p)
-        return cd
+        return cd'''
+        
+class space_carving_2_masks():
+    def __init__(self, dataset_path):
+        self.masks_files = sorted (glob.glob(os.path.join(dataset_path, 'masks', '*.png')) )#get all .png file names from folder path
+        self.extrinsics = self.load_extrinsics(os.path.join(dataset_path, 'extrinsics'))
+        #self.bbox = json.load(open(os.path.join(dataset_path, 'bbox.json')))
+        self.bbox = json.load(open(os.path.join(dataset_path, '/home/pico/uni/romi/scanner_cube/bbox_min_max.json')))
+        self.camera_model = json.load(open(os.path.join(dataset_path, 'camera_model.json')))
+        self.intrinsics= self.camera_model['params'][0:4]
+        
+        params = json.load(open(os.path.join(dataset_path, 'params.json')))
+        #self.gt=o3d.io.read_point_cloud(params["gt_path"])
+        #self.gt_points = np.asarray(self.gt.points)
+        self.n_dilation=params["sc"]["n_dilation"]
+        self.voxel_size = params['sc']['voxel_size']
+        
+        self.set_sc(self.bbox)
+        
+    def reset(self):
+        del(self.sc)
+        self.set_sc(self.bbox) 
+        
+    def load_extrinsics(self,path):
+        ext = []
+        ext_files = glob.glob(os.path.join(path, '*.json'))
+        assert len(ext_files) != 0,"json list is empty."
+        for i in sorted(ext_files):                                                                                                                                     
+            ext.append(json.load(open(i)))                                                                                                                                                                                                                                                  
+        return ext 
+    
+    def load_mask(self,idx):                                                                                                                                         
+        img = cv2.imread(self.masks_files[idx], cv2.IMREAD_GRAYSCALE)                                                                                                                                                                                                                                                                                                                                                                                     
+        return img
+
+    def set_sc(self,bbox):
+        x_min, x_max = bbox['x']
+        y_min, y_max = bbox['y']
+        z_min, z_max = bbox['z']
+
+        nx = int((x_max - x_min) / self.voxel_size) + 1
+        ny = int((y_max - y_min) / self.voxel_size) + 1
+        nz = int((z_max - z_min) / self.voxel_size) + 1
+
+        self.origin = np.array([x_min, y_min, z_min])
+        self.sc = cl.Backprojection([nx, ny, nz], [x_min, y_min, z_min], self.voxel_size)
+
+    def carve(self,idx):
+        im = self.load_mask(idx)
+        self.space_carve(im, self.extrinsics[idx])
+        
+    def space_carve(self, mask, rt):
+        #mask = im.copy() #get_mask(im)
+        rot = sum(rt['R'], [])
+        tvec = rt['T']
+        if self.n_dilation:
+            for k in range(self.n_dilation): mask = binary_dilation(mask)    
+        self.sc.process_view(self.intrinsics, rot, tvec, mask)
+        
+    '''def dist_to_gt(self):
+        vol = self.sc.values().copy()
+        vol = vol.reshape(self.sc.shape)
+        pcd=proc3d.vol2pcd_exp(vol, self.origin, self.voxel_size, level_set_value=0) 
+        pcd_p = np.asarray(pcd.points)
+        cd=chamfer_d(self.gt_points , pcd_p)
+        return cd'''
+
     
     
     
